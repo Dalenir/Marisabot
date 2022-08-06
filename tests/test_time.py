1e58120fc09b3d343ed8756c11b15eb6733ea604
Borodur <60537405+Dalenir@users.noreply.github.com>
2022-08-06 14:58:42 +0300

    Тесты в контейнере

--- tests/test_time.py
+++ tests/test_time.py
@@ -9,8 +9,10 @@ from DBs import DBuse
 class TestTime:
 
     async def test_no_time(self):
-        assert await DBuse.time_watcher() is False
+        assert await DBuse.time_watcher() is not False
 
+    async def test_redis_connection(self):
+        assert await DBuse.redis_get('key') is None
 
 if __name__ == '__main__':
     unittest.main()
