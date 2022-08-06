c86c381a1ff613d54e263e961ce7bb4467145a3d
Borodur <60537405+Dalenir@users.noreply.github.com>
2022-08-06 19:18:22 +0300

    z

--- tests/test_messages.py
+++ tests/test_messages.py
@@ -18,7 +18,7 @@ class TestMessages:
         dp = Dispatcher()
         dp.include_router(test_hand.router)
         result = await dp.feed_update(bot, test_message)
-        assert result == 'Smoketesting was sudccessfull'
+        assert result == 'Smoketesting was successfull'
 
 
 if __name__ == '__main__':
