a7b3c21ad0cb582bfc2280185de403bd650006c5
Borodur <60537405+Dalenir@users.noreply.github.com>
2023-03-11 14:20:51 +0400

    Update bot_logger.py

--- AllLogs/bot_logger.py
+++ AllLogs/bot_logger.py
@@ -1,5 +1,9 @@
 import logging
+import os
 import pathlib
+import socket
+import sys
+import traceback
 from datetime import datetime
 from colorama import init, Fore, Back
 from aiogram import loggers
@@ -36,10 +40,27 @@ class BotLogger:
         self.infolog.setLevel(logging.INFO)
 
         self.infolog.addHandler(logging.FileHandler(filename=f"AllLogs/bot_logs/{today_for_log}.log", mode='a'))
-        color_formatter = ColorFormatter("%(name)-10s %(levelname)-18s %(message)s")
+        color_formatter = ColorFormatter(f"%(name)-10s %(levelname)-18s %(message)s")
         console = logging.StreamHandler()
         console.setFormatter(color_formatter)
         self.infolog.addHandler(console)
+        if os.getenv("PROD_LOGS"):
+            sys.excepthook = BotLogger.exception_logging
+
+    @staticmethod
+    def exception_logging(exctype, value, tb):
+        """
+        Log exception by using the root logger.
+
+        Parameters
+        ----------
+        exctype : type
+        value : NameError
+        tb : traceback
+        """
+        write_val = {'exception_type': str(exctype),
+                     'message': str(traceback.format_tb(tb, 10))}
+        logging.exception(str(write_val))
 
 
 main_logger = BotLogger()
